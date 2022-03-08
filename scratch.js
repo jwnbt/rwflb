269c2d5d528afd29618b122af7420ecee490fdda
jwnbt <jwnbtjun@gmail.com>
2022-03-08 16:24:55 -0600

    init

---
+++ scratch.js
@@ -0,0 +1,6 @@
+let a = {};
+let b = { a: "a", b: "b" };
+let c = { c: "c", d: "d" };
+let d = { e: "e", f: "f" };
+
+console.log({ ...a, ...b, ...c, ...d });
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+const items = [
+  { name: "Hamburger", price: 15.99, description: "A deliecious burger." },
+  { name: "Pizza", price: 11.99, description: "A deliecious pizza." },
+  { name: "Pho", price: 15.99, description: "A deliecious Pho." },
+];
+
+const cart = [];
+function ItemCard(props) {
+  const [QTY, setQTY] = useState("");
+  const [itemCount, setItemCount] = useState(1);
+  const handleAdd = (quantity) => {
+    setItemCount(itemCount + 1);
+    setQTY(quantity);
+    props.onAddItem((currentItems) => {
+      return [...currentItems, { itemCount, ...props.item, QTY }];
+    });
+  };
+  return (
+    <div className="item-card">
+      <Item item={props.item} />
+      <QuantityOrder onAdd={handleAdd} />
+    </div>
+  );
+}
+
+function ItemCards(props) {
+  const [completeOrder, setCompleteOrder] = useState([]);
+  return (
+    <div className="container">
+      {items.map((item, index) => {
+        return (
+          <ItemCard key={index} item={item} onAddItem={setCompleteOrder} />
+        );
+      })}
+    </div>
+  );
+}
+
+function QuantityOrder(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onAdd(event.target.quantity.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select name="quantity" id="quantity">
+          <option value="qty">qty</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <div>
+      <div>{props.item.name}</div>
+      <div>{props.item.price}</div>
+      <div>{props.item.description}</div>
+    </div>
+  );
+}
+
+function ItemList() {
+  return <></>;
+}
+
+function App() {
+  const reffy = useRef(null);
+  console.log(reffy);
+  return <ItemCards />;
+}
+
+export default App;
