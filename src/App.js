9d6f13b7202326193669cdc19ef84dbd225252a9
jwnbt <jwnbtjun@gmail.com>
2022-03-09 06:17:00 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState("click me");
   const handleClick = () => {
     setText(Math.random());
   };
