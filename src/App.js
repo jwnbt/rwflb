9b36c684d7e6ac4d52de82d17acc19d0d51385c1
jwnbt <jwnbtjun@gmail.com>
2022-03-12 05:46:06 -0600

    moved goals table to own component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-//test
+
 function AddGoal({ onAdd }) {
   const [goal, setGoal] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     goal !== "" && onAdd({ name: goal });
     setGoal("");
   };
+  const handleChange = (event) => setGoal(event.target.value);
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        className="input"
-        type="text"
-        value={goal}
-        placeholder="enter a goal to track"
-        onChange={(e) => setGoal(e.target.value)}
-      />
-      <input className="submitbtn" type="submit" value="add" />
+      <input className="input" type="text" value={goal} placeholder="enter a goal to track" onChange={handleChange} />
+      <input className="submit-btn" type="submit" value="add" />
     </form>
   );
-}
-
-function Goal(props) {
-  return <div>{props.goalName}</div>;
 }
 
 function Goals() {
@@ -31,31 +22,37 @@ function Goals() {
   const handleAdd = (goal) => {
     setGoals((prevGoals) => [...prevGoals, goal]);
   };
-  const handleClick = (goalId) => {};
-
   return (
     <div className="console">
       <AddGoal onAdd={handleAdd} />
       <div className="cur-goal-label">{"Current Goals Tracking"}</div>
-      <table className="goal">
-        <tbody>
-          {goals.map((goal, index) => {
-            return (
-              <tr key={index}>
-                <td style={{ width: "100%", fontSize: "20px" }}>
-                  <Goal goalName={goal.name} />
-                </td>
-                <td style={{ width: "100%", fontSize: "20px" }}>
-                  <button type="button" onClick={handleClick(index)}>
-                    done
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <GoalsTable goals={goals} />
     </div>
+  );
+}
+
+function Goal(props) {
+  return <div>{props.goalName}</div>;
+}
+
+function GoalsTable({ goals }) {
+  return (
+    <table className="goal">
+      <tbody>
+        {goals.map((goal, index) => {
+          return (
+            <tr key={index}>
+              <td style={{ width: "100%", fontSize: "20px" }}>
+                <Goal goalName={goal.name} />
+              </td>
+              <td style={{ width: "100%", fontSize: "20px" }}>
+                <button type="button">{"done"}</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
