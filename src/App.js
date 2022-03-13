7ffa8a57f5e7e9d486e7c83de7f2846216b1ca08
jwnbt <jwnbtjun@gmail.com>
2022-03-13 15:27:45 -0500

    add done not done tables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 
+const data = [
+  { name: "workout", done: false, data: null },
+  { name: "eat", done: false, data: null },
+  { name: "study", done: false, data: null },
+  { name: "clean", done: true, data: "20220313" },
+  { name: "play", done: true, data: "20220313" },
+  { name: "read", done: true, data: "20220313" }
+];
+
 function AddGoal({ onAdd }) {
   const [goal, setGoal] = useState("");
   const handleSubmit = (event) => {
@@ -17,39 +26,43 @@ function AddGoal({ onAdd }) {
   );
 }
 
-function Goals() {
-  const [goals, setGoals] = useState([]);
+function GoalsConsole() {
+  const [goals, setGoals] = useState(data);
   const handleAdd = (goal) => {
     setGoals((prevGoals) => [...prevGoals, goal]);
+  };
+  const handleAction = (goal) => {
+    goal.done = !goal.done;
   };
   return (
     <div className="console">
       <AddGoal onAdd={handleAdd} />
-      <div className="cur-goal-label">{"Current Goals Tracking"}</div>
-      <GoalsTable goals={goals} />
+      <div className="label">{"not done goals"}</div>
+      <GoalsTable goals={goals.filter((goal) => !goal.done)} onAction={handleAction} />
+      <div className="label">{"done goals"}</div>
+      <GoalsTable goals={goals.filter((goal) => goal.done)} />
     </div>
   );
 }
 
-function Goal(props) {
-  return <div>{props.goalName}</div>;
+function GoalRow({ goal }) {
+  return (
+    <tr>
+      <td>{goal.name}</td>
+      <td>
+        {!goal.done && <button className="done-btn">{"done"}</button>}
+        {goal.done && <button className="undo-btn">{"undo"}</button>}
+      </td>
+    </tr>
+  );
 }
 
 function GoalsTable({ goals }) {
   return (
-    <table className="goal">
+    <table className="goal-tbl">
       <tbody>
-        {goals.map((goal, index) => {
-          return (
-            <tr key={index}>
-              <td style={{ width: "100%", fontSize: "20px" }}>
-                <Goal goalName={goal.name} />
-              </td>
-              <td style={{ width: "100%", fontSize: "20px" }}>
-                <button type="button">{"done"}</button>
-              </td>
-            </tr>
-          );
+        {goals.map((g, id) => {
+          return <GoalRow key={id} goal={g} />;
         })}
       </tbody>
     </table>
@@ -59,7 +72,7 @@ function GoalsTable({ goals }) {
 function App() {
   return (
     <div className="app">
-      <Goals />
+      <GoalsConsole />
     </div>
   );
 }
