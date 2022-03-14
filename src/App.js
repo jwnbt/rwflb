93b3a8a5e45db2ee1148cbb9134342d8ecfc516e
jwnbt <jwnbtjun@gmail.com>
2022-03-14 06:46:01 -0500

    removed data obj, add math.random for pkey for POC purpose

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 
-const data = [
-  { pkey: 1, name: "workout", done: false, data: null },
-  { pkey: 2, name: "eat", done: false, data: null },
-  { pkey: 3, name: "study", done: false, data: null },
-  { pkey: 4, name: "clean", done: true, data: "20220313" },
-  { pkey: 5, name: "play", done: true, data: "20220313" },
-  { pkey: 6, name: "read", done: true, data: "20220313" }
-];
-
 function AddAGoal({ onAdd }) {
   const [goal, setGoal] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    goal !== "" && onAdd({ name: goal });
+    goal !== "" && onAdd({ pkey: Math.random(), name: goal, done: false, date: null });
     setGoal("");
   };
   const handleChange = (event) => setGoal(event.target.value);
@@ -27,49 +18,22 @@ function AddAGoal({ onAdd }) {
 }
 
 function GoalsConsole() {
-  const [goals, setGoals] = useState(data);
+  const [goals, setGoals] = useState([]);
   const handleAdd = (goal) => {
     setGoals((prevGoals) => [...prevGoals, goal]);
   };
   const handleClick = (goal) => {
     goal.done = !goal.done;
-    setGoals([...goals.filter((g) => g.pkey !== goal.pkey), goal]);
+    setGoals((prevGoals) => [...prevGoals.filter((g) => g.pkey !== goal.pkey), goal]);
   };
   return (
     <div className="console">
       <AddAGoal onAdd={handleAdd} />
       <div className="label">{"not done goals"}</div>
-      <NotDoneGoalTable notDoneGoals={goals.filter((goal) => !goal.done)} onAction={handleClick} />
+      <NotDoneGoalTable goals={goals.filter((g) => !g.done)} onAction={handleClick} />
       <div className="label">{"done goals"}</div>
-      <DoneGoalTable doneGoals={goals.filter((goal) => goal.done)} onAction={handleClick} />
+      <DoneGoalTable goals={goals.filter((g) => g.done)} onAction={handleClick} />
     </div>
-  );
-}
-
-function NotDoneGoalTable({ notDoneGoals, onAction }) {
-  return <GoalsTable goals={notDoneGoals.filter((goal) => !goal.done)} onAction={onAction} />;
-}
-function DoneGoalTable({ doneGoals, onAction }) {
-  return <GoalsTable goals={doneGoals.filter((goal) => goal.done)} onAction={onAction} />;
-}
-
-function GoalRow({ goal, onAction }) {
-  return (
-    <tr className="goal-row ">
-      <td>{goal.name}</td>
-      <td>
-        {!goal.done && (
-          <button className="done-btn" onClick={() => onAction(goal)}>
-            {"done"}
-          </button>
-        )}
-        {goal.done && (
-          <button className="undo-btn" onClick={() => onAction(goal)}>
-            {"undo"}
-          </button>
-        )}
-      </td>
-    </tr>
   );
 }
 
@@ -82,6 +46,36 @@ function GoalsTable({ goals, onAction }) {
         })}
       </tbody>
     </table>
+  );
+}
+
+function NotDoneGoalTable({ goals, onAction }) {
+  return <GoalsTable goals={goals.filter((g) => !g.done)} onAction={onAction} />;
+}
+function DoneGoalTable({ goals, onAction }) {
+  return <GoalsTable goals={goals.filter((g) => g.done)} onAction={onAction} />;
+}
+
+function GoalRow({ goal, onAction }) {
+  const handleClick = () => {
+    onAction(goal);
+  };
+  return (
+    <tr className="goal-row ">
+      <td>{goal.name}</td>
+      <td>
+        {!goal.done && <GoalActionButton type="done" onClick={handleClick} />}
+        {goal.done && <GoalActionButton type="undo" onClick={handleClick} />}
+      </td>
+    </tr>
+  );
+}
+
+function GoalActionButton({ type, onClick }) {
+  return (
+    <button className={`${type}-btn`} onClick={onClick}>
+      {type}
+    </button>
   );
 }
 
